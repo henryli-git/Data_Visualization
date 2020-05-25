143652ab6ba5746df723acbbf0ca7f43fbb15935
Henry Li <65503735+henryli-git@users.noreply.github.com>
2020-05-24 20:37:16 -0600

    Add files via upload

--- WHO_Obesity_2016/WHO_obesity_by_country_2016.py
+++ WHO_Obesity_2016/WHO_obesity_by_country_2016.py
@@ -17,8 +17,9 @@ both = df['Both.sexes']
 plt.barh(country, both,
          color=['#CD5C5C', '#00BFFF', '#FF0000', '#FA8072', '#6495ED', '#F08080', '#FF6347', '#0000FF', '#B22222'])
 
+plt.title('Obesity Among Adults Around the World in 2016', size=10)
 plt.tick_params(axis='y', which='major', labelsize=1.3)
-plt.title('Obesity Among Adults Around the World in 2016', size=12)
-plt.xlabel('% Obese (BMI ≥ 30)', size=10)
-plt.savefig('Obesity_2016_graph.pdf')
+plt.xticks(size=6.5)
+plt.xlabel('% Obese (BMI ≥ 30)', size=8)
+plt.savefig('WHO_obesity_by_country_2016_graph.pdf')
 plt.show()
