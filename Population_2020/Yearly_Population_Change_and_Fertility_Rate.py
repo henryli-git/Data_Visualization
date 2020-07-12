6164b483b9e5f9acfdedf2d1d7d33c6df461639a
Henry Li <henry.li.github@gmail.com>
2020-07-11 21:42:19 -0600

    Add files via upload

---
+++ Population_2020/Yearly_Population_Change_and_Fertility_Rate.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from adjustText import adjust_text
+
+df = pd.read_csv('population_by_country_2020.csv')
+df = df.sort_values('Population (2020)', ascending=False)
+df = df[:50]
+df = df[~df.isin(['N.A.'])]
+df = df.fillna(0)
+df['Fert. Rate'] = df['Fert. Rate'].astype(float)
+df['Yearly Change'] = df['Yearly Change'].str.split('%', expand=True)
+df['Yearly Change'] = df['Yearly Change'].astype(float)
+
+x = df['Yearly Change']
+y = df['Fert. Rate']
+z = df['Country (or dependency)'].values
+plt.style.use('seaborn-dark')
+plt.rcParams['font.family'] = 'Meiryo'
+plt.figure(figsize=(16,9))
+plt.scatter(x, y, color='#228B22', alpha=.8)
+texts = [plt.text(x[i], y[i], text, color='#0887BE') for i, text in enumerate(z)]
+adjust_text(texts,arrowprops=dict(arrowstyle='-', color='#8B4513', alpha=.7))
+plt.title("Yearly Population Change and Fertility Rate for the 50 Most Populous Countries", fontsize=18, fontweight='bold', color='#045F86')
+plt.xlabel('Yearly Population Change (%)', fontsize=15, fontweight='bold', color='#045F86')
+plt.xticks(fontsize=12, color='#045F86')
+plt.ylabel('Fertility Rate', fontsize=15, fontweight='bold', color='#045F86')
+plt.yticks(fontsize=12, color='#045F86')
+plt.gca().get_xticklabels()[1].set_color('red')
+plt.tight_layout()
+
+plt.savefig('Yearly_Population_Change_and_Fertility_Rate_graph.png', ppi=300)
+plt.show()
+
