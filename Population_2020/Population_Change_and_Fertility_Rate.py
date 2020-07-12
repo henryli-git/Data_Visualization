f6a7e15d06792f07512417d7a48f170244f5ea37
Henry Li <henry.li.github@gmail.com>
2020-07-12 09:55:22 -0600

    Add files via upload

--- Population_2020/Population_Change_and_Fertility_Rate.py
+++ Population_2020/Population_Change_and_Fertility_Rate.py
@@ -16,11 +16,12 @@ y = df['Fert. Rate']
 z = df['Country (or dependency)'].values
 plt.style.use('seaborn-dark')
 plt.rcParams['font.family'] = 'Meiryo'
-plt.figure(figsize=(16,9))
+plt.figure(figsize=(16, 9))
 plt.scatter(x, y, color='#228B22', alpha=.8)
 texts = [plt.text(x[i], y[i], text, color='#0887BE') for i, text in enumerate(z)]
-adjust_text(texts,arrowprops=dict(arrowstyle='-', color='#8B4513', alpha=.7))
-plt.title("Population Change and Fertility Rate for the 50 Most Populous Countries in 2020", fontsize=18, fontweight='bold', color='#045F86')
+adjust_text(texts, arrowprops=dict(arrowstyle='-', color='#8B4513', alpha=.7))
+plt.title("Population Change and Fertility Rate for the 50 Most Populous Countries in 2020", fontsize=18,
+          fontweight='bold', color='#045F86')
 plt.xlabel('Annual Population Change (%)', fontsize=15, fontweight='bold', color='#045F86')
 plt.xticks(fontsize=12, color='#045F86')
 plt.ylabel('Fertility Rate', fontsize=15, fontweight='bold', color='#045F86')
@@ -30,4 +31,3 @@ plt.tight_layout()
 
 plt.savefig('Population_Change_and_Fertility_Rate_graph.png', ppi=600)
 plt.show()
-
